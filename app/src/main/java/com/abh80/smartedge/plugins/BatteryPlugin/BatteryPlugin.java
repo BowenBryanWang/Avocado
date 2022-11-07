e3c294b4f01aa1283dc9eb35da6bd9600cd4c801
Saltyp0rridge <wbw20@mails.tsinghua.edu.cn>
2022-11-07 12:06:42 +0800

    添加注释方便理解

--- app/src/main/java/com/abh80/smartedge/plugins/BatteryPlugin/BatteryPlugin.java
+++ app/src/main/java/com/abh80/smartedge/plugins/BatteryPlugin/BatteryPlugin.java
@@ -34,59 +34,69 @@ public class BatteryPlugin extends BasePlugin {
     }
 
     private OverlayService ctx;
+    //这里的OverlayService是一个Service，用于显示悬浮窗，可以理解为一个Activity
 
     @Override
     public void onCreate(OverlayService context) {
         ctx = context;
         ctx.registerReceiver(mBroadcastReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        //这里的intentFilter是一个action，这个action是系统定义好的，我们只要监听这个action就可以了
     }
 
     private View mView;
+    //这里定义的是一个view，这个view就是我们要显示的view，也就是battery的view
 
     @Override
-    public View onBind() {
+    public View onBind() //onbind函数的作用是获取我们要显示的view
+
+    {
         mView = LayoutInflater.from(ctx).inflate(R.layout.battery_layout, null);
+        //这里的LayoutInflater.from(ctx).inflate(R.layout.battery_layout, null)的作用是获取我们的battery_layout.xml文件
         init();
+        //init函数的作用是初始化我们的view
         return mView;
     }
 
     private void init() {
         tv = mView.findViewById(R.id.text_percent);
+        //这里的mView.findViewById(R.id.text_percent)的作用是获取我们的battery_layout.xml文件中的text_percent这个id
         batteryImageView = mView.findViewById(R.id.cover);
+        //这里的R.id.cover指的是我们的battery_layout.xml文件中的cover这个id
+        //获取完之后，我们就可以对这个view进行操作了
         updateView();
     }
 
     float batteryPercent;
     private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
-        public void onReceive(Context context, Intent intent) {
-            int status = intent.getExtras().getInt(BatteryManager.EXTRA_STATUS);
-            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING;
+        public void onReceive(Context context, Intent intent) {//这里的onReceive函数的作用是监听我们的电量
+            int status = intent.getExtras().getInt(BatteryManager.EXTRA_STATUS);//status是电量的状态
+            boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING;//isCharging是一个boolean值，用于判断是否在充电
             if (isCharging) {
-                ctx.enqueue(BatteryPlugin.this);
-                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-                batteryPercent = level * 100 / (float) scale;
-                updateView();
+                ctx.enqueue(BatteryPlugin.this);//这里的ctx.enqueue(BatteryPlugin.this)的作用是将我们的view显示出来
+                int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);//这里的level是电量的百分比
+                int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);//这里的scale是电量的最大值
+                batteryPercent = level * 100 / (float) scale;//这里的batteryPercent是电量的百分比
+                updateView();//这里的updateView()的作用是更新我们的view
             } else {
-                ctx.dequeue(BatteryPlugin.this);
-                if (tv != null && batteryImageView != null) {
-                    ValueAnimator valueAnimator = ValueAnimator.ofInt(0, ctx.dpToInt(0));
-                    valueAnimator.setDuration(300);
-                    valueAnimator.addUpdateListener(valueAnimator1 -> {
-                        ViewGroup.LayoutParams p = batteryImageView.getLayoutParams();
+                ctx.dequeue(BatteryPlugin.this);//这里的ctx.dequeue(BatteryPlugin.this)的作用是将我们的view隐藏起来
+                if (tv != null && batteryImageView != null) {//这里的tv != null && batteryImageView != null的作用是判断我们的view是否为空
+                    ValueAnimator valueAnimator = ValueAnimator.ofInt(0, ctx.dpToInt(0));//这里的ValueAnimator.ofInt(0, ctx.dpToInt(0))的作用是设置我们的view的动画
+                    valueAnimator.setDuration(300);//这里的valueAnimator.setDuration(300)的作用是设置我们的view的动画时间
+                    valueAnimator.addUpdateListener(valueAnimator1 -> {//这里的valueAnimator.addUpdateListener(valueAnimator1 -> {的作用是设置我们的view的动画监听
+                        ViewGroup.LayoutParams p = batteryImageView.getLayoutParams();//这里的ViewGroup.LayoutParams p = batteryImageView.getLayoutParams()的作用是获取我们的view的LayoutParams
                         p.width = (int) valueAnimator1.getAnimatedValue();
                         p.height = (int) valueAnimator1.getAnimatedValue();
-                        batteryImageView.setLayoutParams(p);
+                        batteryImageView.setLayoutParams(p);//这里的batteryImageView.setLayoutParams(p)的作用是设置我们的view的LayoutParams
                     });
                     valueAnimator.addListener(new AnimatorListenerAdapter() {
                         @Override
-                        public void onAnimationStart(Animator animation) {
-                            super.onAnimationEnd(animation);
-                            tv.setVisibility(View.INVISIBLE);
+                        public void onAnimationStart(Animator animation) {//这里的onAnimationStart(Animator animation)的作用是设置我们的view的动画开始时的监听
+                            super.onAnimationEnd(animation);//这里的super.onAnimationEnd(animation)的作用是设置我们的view的动画结束时的监听
+                            tv.setVisibility(View.INVISIBLE);//这里的tv.setVisibility(View.INVISIBLE)的作用是将我们的view隐藏起来
                         }
                     });
-                    valueAnimator.start();
+                    valueAnimator.start();//这里的valueAnimator.start()的作用是开始我们的view的动画
                 }
             }
         }
@@ -94,10 +104,11 @@ public class BatteryPlugin extends BasePlugin {
     private TextView tv;
     private BatteryImageView batteryImageView;
 
-    private void updateView() {
+    private void updateView()//这个updateView函数的通过当前充电信号之类的来更新我们的view
+    {
         if (mView != null) {
-            tv.setText((int) batteryPercent + "%");
-            batteryImageView.updateBatteryPercent(batteryPercent);
+            tv.setText((int) batteryPercent + "%");//这里的tv.setText((int) batteryPercent + "%")的作用是设置我们的text_percent这个id的text
+            batteryImageView.updateBatteryPercent(batteryPercent);//这里的batteryImageView.updateBatteryPercent(batteryPercent)的作用是设置我们的cover这个id的图片
             if (batteryPercent > 80) {
                 batteryImageView.setStrokeColor(Color.GREEN);
                 tv.setTextColor(Color.GREEN);
--- app/src/main/java/com/abh80/smartedge/plugins/MediaSession/SongVisualizer.java
+++ app/src/main/java/com/abh80/smartedge/plugins/MediaSession/SongVisualizer.java
@@ -28,35 +28,35 @@ public class SongVisualizer extends View {
         init();
     }
 
-    public boolean paused;
-    private byte[] bytes;
+    public boolean paused;//这里定义了一个boolean变量，用于判断是否暂停
+    private byte[] bytes;//这里定义了一个byte数组，用于存储我们的音频数据
 
-    public void setPlayerId(int sessionID) {
+    public void setPlayerId(int sessionID) {//这里定义了一个setPlayerId函数，用于设置我们的音频数据
         try {
             if (visualizer != null) {
-                release();
+                release();//这里的release函数的作用是释放visualizer
                 visualizer = null;
             }
-            visualizer = new Visualizer(sessionID);
-            visualizer.setEnabled(false);
-            visualizer.setScalingMode(Visualizer.SCALING_MODE_NORMALIZED);
-            visualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);
-            visualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {
+            visualizer = new Visualizer(sessionID);//这里的sessionID是
+            visualizer.setEnabled(false);//这里的setEnabled函数的作用是设置visualizer是否可用
+            visualizer.setScalingMode(Visualizer.SCALING_MODE_NORMALIZED);//这里的setScalingMode函数的作用是设置visualizer的缩放模式
+            visualizer.setCaptureSize(Visualizer.getCaptureSizeRange()[1]);//这里的setCaptureSize函数的作用是设置visualizer的采样大小
+            visualizer.setDataCaptureListener(new Visualizer.OnDataCaptureListener() {//这里的setDataCaptureListener函数的作用是设置visualizer的数据监听器
                 @Override
                 public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes,
-                                                  int samplingRate) {
-                    SongVisualizer.this.bytes = bytes;
-                    if (!paused) invalidate();
+                                                  int samplingRate) {//这里的onWaveFormDataCapture函数的作用是设置visualizer的波形数据监听器
+                    SongVisualizer.this.bytes = bytes;//这里的bytes是我们的音频数据
+                    if (!paused) invalidate();//这里的invalidate函数的作用是刷新视图
                 }
 
                 @Override
                 public void onFftDataCapture(Visualizer visualizer, byte[] bytes,
-                                             int samplingRate) {
+                                             int samplingRate) {//这里的onFftDataCapture函数的作用是设置visualizer的FFT数据监听器
                 }
-            }, Visualizer.getMaxCaptureRate() / 2, true, false);
+            }, Visualizer.getMaxCaptureRate() / 2, true, false);//这里的setMaxCaptureRate函数的作用是设置visualizer的采样率
 
 
-            visualizer.setEnabled(true);
+            visualizer.setEnabled(true);//这里的setEnabled函数的作用是设置visualizer是否可用
         } catch (Exception e) {
             // do nothing lol
         }
@@ -73,9 +73,9 @@ public class SongVisualizer extends View {
     }
 
     private void init() {
-        setColor(Color.BLUE);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setStrokeCap(Paint.Cap.ROUND);
+        setColor(Color.BLUE);//这里的setColor函数的作用是设置画笔的颜色
+        paint.setStyle(Paint.Style.FILL);//这里的setStyle函数的作用是设置画笔的样式
+        paint.setStrokeCap(Paint.Cap.ROUND);//这里的setStrokeCap函数的作用是设置画笔的笔触样式
     }
 
     public void setColor(int Color) {
@@ -85,26 +85,26 @@ public class SongVisualizer extends View {
     private final Paint paint = new Paint();
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    protected void onDraw(Canvas canvas) {//这里的onDraw函数的作用是绘制视图
         float density = 8;
 
         if (bytes != null) {
-            float barWidth = getWidth() / density;
-            float div = bytes.length / density;
-            paint.setStrokeWidth(barWidth - 4);
+            float barWidth = getWidth() / density;//这里的getWidth函数的作用是获取视图的宽度
+            float div = bytes.length / density;//这里的bytes.length函数的作用是获取bytes的长度
+            paint.setStrokeWidth(barWidth - 4);//这里的setStrokeWidth函数的作用是设置画笔的笔触宽度
 
-            for (int i = 0; i < density; i++) {
-                int bytePosition = (int) Math.ceil(i * div);
-                float barX = (i * barWidth) + (barWidth / 2);
-                if (bytes[bytePosition] == 0 || bytes[bytePosition] + 128 == 0) {
-                    canvas.drawLine(barX, (getHeight() / 2f), barX, (getHeight() / 2f), paint);
+            for (int i = 0; i < density; i++) {//这里的density是我们的音频数据的长度
+                int bytePosition = (int) Math.ceil(i * div);//这里的ceil函数的作用是向上取整
+                float barX = (i * barWidth) + (barWidth / 2);//这里的barX是我们的音频数据的X坐标
+                if (bytes[bytePosition] == 0 || bytes[bytePosition] + 128 == 0) {//这里的bytes[bytePosition]是我们的音频数据
+                    canvas.drawLine(barX, (getHeight() / 2f), barX, (getHeight() / 2f), paint);//这里的getHeight函数的作用是获取视图的高度
                 } else {
                     int top = (getHeight() - 20) +
-                            ((byte) (Math.abs(bytes[bytePosition]) + 128)) * (getHeight() - 20) / 128;
-                    canvas.drawLine(barX, ((getHeight() + 20) - top) / 2f, barX, top, paint);
+                            ((byte) (Math.abs(bytes[bytePosition]) + 128)) * (getHeight() - 20) / 128;//这里的getHeight函数的作用是获取视图的高度
+                    canvas.drawLine(barX, ((getHeight() + 20) - top) / 2f, barX, top, paint);//这里的getHeight函数的作用是获取视图的高度
                 }
             }
-            super.onDraw(canvas);
+            super.onDraw(canvas);//这里的super.onDraw函数的作用是调用父类的onDraw函数
         }
     }
 }
