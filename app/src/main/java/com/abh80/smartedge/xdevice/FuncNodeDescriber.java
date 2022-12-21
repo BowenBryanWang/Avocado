bb03d1d4e412173dfca1a83dbd9d8ebf0c0eb646
Saltyp0rridge <wbw20@mails.tsinghua.edu.cn>
2022-12-21 14:36:14 +0800

    存档

--- app/src/main/java/com/abh80/smartedge/xdevice/FuncNodeDescriber.java
+++
@@ -1,9 +0,0 @@
-package com.abh80.smartedge.xdevice;
-
-import com.abh80.smartedge.AccessibilityNodeInfoRecord;
-
-public class FuncNodeDescriber extends NodeDescriber {
-    public FuncNodeDescriber(AccessibilityNodeInfoRecord node) {
-        super(node);
-    }
-}
