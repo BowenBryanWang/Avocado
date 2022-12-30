2a75626697ab8cac888db7f57f0c1b945a0ddcc2
Saltyp0rridge <wbw20@mails.tsinghua.edu.cn>
2022-12-30 23:04:40 +0800

    try

---
+++ app/src/main/java/com/Saltyp0rridge/Avocado/plugins/TextPlugin/ResponseData.java
@@ -0,0 +1,23 @@
+package com.Saltyp0rridge.Avocado.plugins.TextPlugin;
+
+public class ResponseData {
+    private String type;
+    private String html;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getHtml() {
+        return html;
+    }
+
+    public void setHtml(String html) {
+        this.html = html;
+    }
+}
+
--- app/src/main/java/com/Saltyp0rridge/Avocado/plugins/TextPlugin/TextPlugin.java
+++ app/src/main/java/com/Saltyp0rridge/Avocado/plugins/TextPlugin/TextPlugin.java
@@ -5,6 +5,8 @@ import static android.content.ContentValues.TAG;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -20,7 +22,9 @@ import com.Saltyp0rridge.Avocado.plugins.BasePlugin;
 import com.Saltyp0rridge.Avocado.services.OverlayService;
 import com.Saltyp0rridge.Avocado.utils.SettingStruct;
 import com.google.android.material.imageview.ShapeableImageView;
-
+//import ResponseData
+import com.google.gson.Gson;
+import com.Saltyp0rridge.Avocado.plugins.TextPlugin.ResponseData;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
@@ -55,8 +59,9 @@ public class TextPlugin extends BasePlugin {
         //尝试ping一下服务器，地址为http://192.168.68.96:5000
         ctx = context;
         mView = LayoutInflater.from(context).inflate(R.layout.text_layout, null);//创建我们的视图
-        mView.findViewById(R.id.blank_space).setVisibility(View.VISIBLE);//设置我们的空白的空间为可见
+        mView.findViewById(R.id.blank_space).setVisibility(View.VISIBLE);//设置我们的空白的空间为可
         init();//初始化我们的插件
+
     }
 
     private View mView;
@@ -76,6 +81,8 @@ public class TextPlugin extends BasePlugin {
     private boolean ishtml = false;
     private boolean isagenda = false;
     private JWebSocketClient client;
+    private String html = "";
+    private String agenda = "";
     private void init() throws URISyntaxException {
         title = mView.findViewById(R.id.title);
         icon  = mView.findViewById(R.id.icon);
@@ -83,9 +90,7 @@ public class TextPlugin extends BasePlugin {
         cover = mView.findViewById(R.id.cover);
         tag = mView.findViewById(R.id.tag);
         inside_text = mView.findViewById(R.id.inside);
-        String BASE_URL = "http://192.168.124.22:5000/test";
-        //1.新建OKHttpClient客户端
-        //1.新建OKHttpClient客户端
+        String BASE_URL = "http://192.168.124.22:5000/detect";
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(1000, TimeUnit.SECONDS)
                 .writeTimeout(1000, TimeUnit.SECONDS)
@@ -94,7 +99,6 @@ public class TextPlugin extends BasePlugin {
 
         //2. 新建一个Request对象
         final Request request = new Request.Builder()
-
                 .url(BASE_URL)
                 .build();
 
@@ -105,76 +109,43 @@ public class TextPlugin extends BasePlugin {
                 try {
                     //3.Response为 OKHttp 中的响应
                     Response response = call.execute();
-                    Log.d(TAG, "run: " + response.body().string());
+                    //返回值是python里的dict，用java获取
+                    String responseBodyString = response.body().string();
+                    Log.d("responseBodyString", responseBodyString);
+                    Gson gson = new Gson();
+                    ResponseData responseData = gson.fromJson(responseBodyString, ResponseData.class);
+                    new Handler(Looper.getMainLooper()).post(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (responseData.getType().equals("html")){
+                                ishtml = true;
+                                html = responseData.getHtml();
+                            }
+                            if (ishtml){
+                                Log.d("ishtml", ishtml + "");
+                                Log.d("url", html);
+                                updateView();
+                            }
+                            return;
+                        }
+                    });
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                return;
             }
         }).start();
-
-
-
-//        try {
-//            System.out.println("mSocket.connected()");
-//            mSocket = IO.socket("http://192.168.124.22:5000/test");
-//            mSocket.connect();
-//            System.out.println(mSocket.connected());
-//        } catch (URISyntaxException e) {
-//            e.printStackTrace();
-//        }
-//        mSocket.emit("message", "hello");
-//        System.out.println("发送成功");
-//
-//        mSocket.on("html", args -> {
-//            inside_text.setText("检测到网址");
-//            tag.setImageDrawable(ctx.getDrawable(R.drawable.html));
-//            ishtml = true;
-//            ctx.enqueue(this);
-//        });
-//        mSocket.on("agenda", args -> {
-//            inside_text.setText("检测到提醒事项");
-//            tag.setImageDrawable(ctx.getDrawable(R.drawable.agenda));
-//            isagenda = true;
-//            ctx.enqueue(this);
-//        });
-        inside_text.setText("检测到网址");
-        tag.setImageDrawable(ctx.getDrawable(R.drawable.html));
-        ishtml = true;
-//        URI uri = URI.create("ws://192.168.124.22:5000/echo");
-//        client = new JWebSocketClient(uri) {
-//            @Override
-//            public void onMessage(String message) {
-//                //message就是接收到的消息
-//                Log.e("JWebSClientService", message);
-//            }
-//        };
-//        try {
-//            client.connectBlocking();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        if (client != null && client.isOpen()) {
-//            client.send("hello");
-//        }
-        ctx.enqueue(this);
-    }
-    /**
-     * 断开连接
-     */
-    private void closeConnect() {
-        try {
-            if (null != client) {
-                client.close();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            client = null;
-        }
     }
 
     private void updateView(){
         if (mView == null) return;
+        ctx.enqueue(this);
+        inside_text.setText("检测到新的网址");
+        tag.setImageDrawable(ctx.getDrawable(R.drawable.html));
+        mView.findViewById(R.id.inside_text).setVisibility(View.VISIBLE);
+
     }
 
 
@@ -197,16 +168,6 @@ public class TextPlugin extends BasePlugin {
         DisplayMetrics metrics = ctx.metrics;
         ctx.animateOverlay2(ctx.dpToInt(100), metrics.widthPixels - ctx.dpToInt(15), expanded);
         animateChild(true, ctx.dpToInt(76));
-//        if (ishtml){
-//            title.setText("检测到网址");
-//            Desc.setText("网页描述");
-//            icon.setImageDrawable(ctx.getDrawable(R.drawable.html));
-//        }
-//        else if (isagenda){
-//            title.setText("检测到提醒事项");
-//            Desc.setText("提醒事项描述");
-//            icon.setImageDrawable(ctx.getDrawable(R.drawable.agenda));
-//        }
         mView.findViewById(R.id.content).setVisibility(View.VISIBLE);
         mView.findViewById(R.id.inside_text).setVisibility(View.GONE);
 
