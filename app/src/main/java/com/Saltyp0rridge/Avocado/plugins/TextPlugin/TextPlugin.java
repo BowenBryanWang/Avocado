9e49ee0c4132690536a724f4cb762dff31a56b99
Saltyp0rridge <wbw20@mails.tsinghua.edu.cn>
2022-12-30 22:04:30 +0800

    try

--- app/src/main/java/com/Saltyp0rridge/Avocado/plugins/TextPlugin/TextPlugin.java
+++ app/src/main/java/com/Saltyp0rridge/Avocado/plugins/TextPlugin/TextPlugin.java
@@ -1,5 +1,7 @@
 package com.Saltyp0rridge.Avocado.plugins.TextPlugin;
 
+import static android.content.ContentValues.TAG;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
@@ -19,12 +21,21 @@ import com.Saltyp0rridge.Avocado.services.OverlayService;
 import com.Saltyp0rridge.Avocado.utils.SettingStruct;
 import com.google.android.material.imageview.ShapeableImageView;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class TextPlugin extends BasePlugin {
     @Override
@@ -72,29 +83,60 @@ public class TextPlugin extends BasePlugin {
         cover = mView.findViewById(R.id.cover);
         tag = mView.findViewById(R.id.tag);
         inside_text = mView.findViewById(R.id.inside);
-        try {
-            System.out.println("mSocket.connected()");
-            mSocket = IO.socket("http://192.168.124.22:5000/test");
-            mSocket.connect();
-            System.out.println(mSocket.connected());
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        mSocket.emit("message", "hello");
-        System.out.println("发送成功");
+        String BASE_URL = "http://192.168.124.22:5000/test";
+        //1.新建OKHttpClient客户端
+        //1.新建OKHttpClient客户端
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(1000, TimeUnit.SECONDS)
+                .writeTimeout(1000, TimeUnit.SECONDS)
+                .readTimeout(1000, TimeUnit.SECONDS)
+                .build();
 
-        mSocket.on("html", args -> {
-            inside_text.setText("检测到网址");
-            tag.setImageDrawable(ctx.getDrawable(R.drawable.html));
-            ishtml = true;
-            ctx.enqueue(this);
-        });
-        mSocket.on("agenda", args -> {
-            inside_text.setText("检测到提醒事项");
-            tag.setImageDrawable(ctx.getDrawable(R.drawable.agenda));
-            isagenda = true;
-            ctx.enqueue(this);
-        });
+        //2. 新建一个Request对象
+        final Request request = new Request.Builder()
+
+                .url(BASE_URL)
+                .build();
+
+        final Call call = okHttpClient.newCall(request);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //3.Response为 OKHttp 中的响应
+                    Response response = call.execute();
+                    Log.d(TAG, "run: " + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+//        try {
+//            System.out.println("mSocket.connected()");
+//            mSocket = IO.socket("http://192.168.124.22:5000/test");
+//            mSocket.connect();
+//            System.out.println(mSocket.connected());
+//        } catch (URISyntaxException e) {
+//            e.printStackTrace();
+//        }
+//        mSocket.emit("message", "hello");
+//        System.out.println("发送成功");
+//
+//        mSocket.on("html", args -> {
+//            inside_text.setText("检测到网址");
+//            tag.setImageDrawable(ctx.getDrawable(R.drawable.html));
+//            ishtml = true;
+//            ctx.enqueue(this);
+//        });
+//        mSocket.on("agenda", args -> {
+//            inside_text.setText("检测到提醒事项");
+//            tag.setImageDrawable(ctx.getDrawable(R.drawable.agenda));
+//            isagenda = true;
+//            ctx.enqueue(this);
+//        });
         inside_text.setText("检测到网址");
         tag.setImageDrawable(ctx.getDrawable(R.drawable.html));
         ishtml = true;
